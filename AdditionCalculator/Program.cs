02a372ca29112a333e34ed1bc6f0cda025b9e2dc
gallivantour <mahendragalli@gmail.com>
2024-08-11 03:12:15 +1000

    First Commit
    i am writing 1st commit because thats what the youtuber did. I'm guessing once i hit "commit to main", my code is gonna be uploaded to github. Lets see.

---
+++ AdditionCalculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Addition Calculator");
+
+            // Ask user to input 1st number
+            Console.Write("Input the 1st number:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            // ask user to input 2nd number
+            Console.Write("imput 2nd number:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            //calculation
+            int sum = a * b;
+            
+            
+            Console.WriteLine($"sum of your numbers is {sum}");
+            Console.Read();
+        }
+    }
+}
